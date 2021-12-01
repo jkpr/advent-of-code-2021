66bb2b65f3a3a0f81dfc5440409d8759a14b4f4b
James <jameskpringle@gmail.com>
2021-12-01 00:41:33 -0600

    Improve Day 1 to one liner

--- aoc2021/day01/__init__.py
+++ aoc2021/day01/__init__.py
@@ -3,20 +3,12 @@ from aoc2021.utils import lines_to_int
 
 def part1(lines: list[str]):
     ints = lines_to_int(lines)
-    count = 0
-    for a, b in zip(ints, ints[1:]):
-        if b > a:
-            count += 1
-    return count
+    return sum(b > a for a, b in zip(ints, ints[1:]))
 
 
 def part2(lines: list[str]):
     ints = lines_to_int(lines)
-    triples = []
-    for a, b, c in zip(ints, ints[1:], ints[2:]):
-        triples.append(a + b + c)
-    count = 0
-    for a, b in zip(triples, triples[1:]):
-        if b > a:
-            count += 1
-    return count
+    return sum(
+        sum(items[1:]) > sum(items[:-1])
+        for items in zip(ints, ints[1:], ints[2:], ints[3:])
+    )
