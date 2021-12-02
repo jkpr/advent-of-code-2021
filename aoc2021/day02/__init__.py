8957c1a9fe91abec58ac6c33e17554f02bbaf08b
James <jameskpringle@gmail.com>
2021-12-02 12:35:02 -0600

    Add Day 2 solution

---
+++ aoc2021/day02/__init__.py
@@ -0,0 +1,32 @@
+def part1(lines: list[str]):
+    depth = 0
+    length = 0
+    for line in lines:
+        parts = line.split()
+        direction = parts[0]
+        distance = int(parts[1])
+        if direction == "forward":
+            length += distance
+        elif direction == "down":
+            depth += distance
+        elif direction == "up":
+            depth -= distance
+    return depth * length
+
+
+def part2(lines: list[str]):
+    aim = 0
+    depth = 0
+    length = 0
+    for line in lines:
+        parts = line.split()
+        direction = parts[0]
+        distance = int(parts[1])
+        if direction == "forward":
+            length += distance
+            depth += aim*distance
+        elif direction == "down":
+            aim += distance
+        elif direction == "up":
+            aim -= distance
+    return depth * length
