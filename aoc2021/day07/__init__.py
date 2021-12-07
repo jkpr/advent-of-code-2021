0b006933a7e219fad0a669b62b4b137a94b95828
James K. Pringle <jameskpringle@gmail.com>
2021-12-07 11:48:39 -0600

    Small improvement with generator

--- aoc2021/day07/__init__.py
+++ aoc2021/day07/__init__.py
@@ -1,11 +1,8 @@
 def move_crabs(lines: list[str], fuel_cost: callable) -> int:
     crabs = [int(i) for i in lines[0].split(",")]
     total_fuel = []
-    for i in range(max(crabs)):
-        fuel_for_crab = []
-        for crab in crabs:
-            fuel_for_crab.append(fuel_cost(i, crab))
-        total_fuel.append(sum(fuel_for_crab))
+    for i in range(min(crabs), max(crabs) + 1):
+        total_fuel.append(sum(fuel_cost(i, crab) for crab in crabs))
     return min(total_fuel)
 
 
